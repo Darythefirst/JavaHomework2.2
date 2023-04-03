cfc586ca53a7a8c71061f3b033dbe3e42f5c0ded
Dary <Dafna_pelmeshka@mail.ru>
2023-04-04 01:11:04 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        int mobileAccount = 100;  //начальная сумма на счете абонента
+        int payment = 1100;    // сумма пополнения
+        int bonusRub;   //сумма бонусных рублей
+        int totalAccount;   //итоговая сумма
+
+        if (payment >= 1000) {
+            bonusRub = payment * 1 / 100;
+            totalAccount = mobileAccount + payment + bonusRub;
+            System.out.println(totalAccount + " Итоговая сумма на счете абонента");
+            System.out.println(bonusRub + " Сумма начисленных бонусных рублей");
+        } else {
+            bonusRub = 0;
+            totalAccount = mobileAccount + payment + bonusRub;
+            System.out.println(totalAccount + " Итоговая сумма на счете абонента");
+            System.out.println(bonusRub + " Сумма начисленных бонусных рублей");
+        }
+
+    }
+}
